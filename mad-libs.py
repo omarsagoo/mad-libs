bd4f28e8568aadc0b2a772b307edab8c921c3ca9
omarsagoo <om.sagoo@gmail.com>
2019-08-29 16:48:14 -0700

    creates userInput function

--- mad-libs.py
+++ mad-libs.py
@@ -1,4 +1,4 @@
-import random
+from random import shuffle
 
 def actionStory():
     nouns = list()
@@ -7,35 +7,34 @@ def actionStory():
     professions = list()
     names = list()
     print('I see you have chosen to make an action story with me today! What a good choice ' + name + '!')
-    print('So lets start!')
-    print('Lets make a list of 5 nouns to use in the story, they can be anything but lets try to make them action related.')
-    for noun in range(5):
-        nouns.append(input('-->'))
+    print('So let\'s start!')
+    
+    print('Let\'s make a list of 5 nouns to use in the story, they can be anything but let\'s try to make them action related.')
+    userInput(nouns)
     
     print('Good! now we need to create some adjectives! give me 5 Adjectives to use in the story.')
-    for adjective in range(5): 
-        adjectives.append(input('-->'))
+    userInput(adjectives)
     
     print('Just a few more steps! lets now list out some verbs to use!')
-    for verb in range(5):
-        verbs.append(input('-->'))
+    userInput(verbs)
    
     print('Now give me 5 professions.')
-    for profession in range(5):
-        professions.append(input('-->'))
+    userInput(professions)
     
     print('Now lastly give me some names for the charecters. 5 names would be great!')
-    for newName in range(5):
-        names.append(input())
+    userInput(names)
 
-    randomizedProfessionals(names, professions)
+    randomize(names)
+    randomize(professions)
     
-    
+#function for randomizing the pos inputs
+def randomize(raw_lists):
+    shuffle(raw_lists)
 
-def randomizedProfessionals(names, professions):
-    random.shuffle(names)
-    random.shuffle(professions)
-
+#function for collecting user input and storing it in the variable
+def userInput(pos):
+    for _ in range(5):
+        pos.append(input('-->'))
 
 print('Hello! What is your name?')
 name = input('-->')
